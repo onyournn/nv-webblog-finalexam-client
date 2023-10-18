9a442117fff5c592d8f743cec390cb07c21007e0
onyournn <saritam64@nu.ac.th>
2023-10-18 14:05:33 +0700

    page1

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import CourseIndex from '@/components/Course/Index'
+import CourseCreate from '@/components/Course/CreateCourse'
 import CourseEdit from '@/components/Course/EditCourse'
 import CourseShow from '@/components/Course/ShowCourse'
-import CourseCreate from '@/components/Course/CreateCourse'
+import CourseIndex from '@/components/Course/Index'
+
 
 Vue.use(Router)
 
